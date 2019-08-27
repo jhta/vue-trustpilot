334f194ebfd1aa010d2753ffa00e44eb26f5a915
Jeison Higuita <jeison.higuita.sanchez@cg.nl>
2019-08-27 09:21:48 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import { isEmpty, forEach, get } from 'lodash'
+
+import TrustpilotWidget from './widget.vue'
+
+// implementation to prevent unnecessary logs in production
+const logger = (msg) => {
+  // `get` is necessary as fallback for non ssr access.
+  const env = get(process, 'env.NODE_ENV')
+  if (env === 'prod' || env == 'production') {
+    return;
+  }
+
+  console.info(msg)
+}
+
+export default {
+  /**
+   * @param {Object} Vue Vue instance
+   * @param {Object} options.widgets Map of widgets with Trustpilot configuration
+   */
+  install(Vue, options = {}) {
+
+    // widgets are required for run it
+    if (isEmpty(options) || isEmpty(options.widgets)) {
+      logger("'vue-trustpilot': Trustpilot widgets haven't been found")
+      return;
+    }
+
+    const { widgets } = options
+
+    // `templateId` and `businessunitId` are required but those could be set as html attributes too.
+    forEach(widgets, (key, value) => {
+      if (!value.hasOwnProperty('templateId')) {
+        logger(`'vue-trustpilot': ${key} widget doesn't have 'templateId' attribute`)
+      }
+
+      if (!value.hasOwnProperty('businessunitId')) {
+        logger(`'vue-trustpilot': ${key} widget doesn't have 'bussinessunitId' attribute`)
+      }
+    })
+
+    // method for access to the widgets information based on the widget identifier
+    Vue.prototype.$trustpilot = (identifier = '') => widgets[identifier] || null
+
+    // Available widget list
+    Vue.prototype.$trustpilotWidgetList = () => Object.keys(widgets)
+
+    // components to access to the widgets information.
+    Vue.component('vue-trustpilot', TrustpilotWidget)
+  }
+}
---
+++ widget.vue
@@ -0,0 +1,52 @@
+<script>
+
+import Vue from 'vue';
+import { kebabCase, reduce } from 'lodash';
+
+export default {
+  props: {
+    /** widget identifier */
+    identifier: {
+      type: String,
+      required: true,
+    },
+  },
+  render(createElement) {
+    const { $attrs, $trustpilot, identifier } = this;
+
+    // access and get the widget configuration based on a name identifier
+    const widget = $trustpilot(identifier);
+
+    // format configuration to data- + kebab-case, format needed for trustpilot script
+    // example: templateId => data-template-id
+    const formattedTrustPilotConfig = reduce(widget, (result, value, key) => ({
+      ...result,
+      [`data-${kebabCase(key)}`]: value,
+    }), {});
+
+    // formatted attributes to assing
+    const attrs = {
+      ...Object.create($attrs),
+      ...formattedTrustPilotConfig,
+      class: 'trustpilot-widget',
+    };
+
+    // anchor link element with url to review
+    // result: <a href="reviewUrl" target="_blank"/>
+    const link = createElement(
+      'a',
+      {
+        attrs: {
+          href: widget.reviewUrl,
+          target: '_blank',
+        },
+      },
+    );
+
+    // result: <div class="trustpilot-widget" data-[key]="[value]" ><a href="reviewUrl" target="_blank" /></div>
+    return createElement('div', { attrs }, [link]);
+  },
+};
+
+
+</script>
